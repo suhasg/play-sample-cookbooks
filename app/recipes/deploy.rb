0fe56894dffd3698e5e35228376083d148bb622c
suhas <suhasg87@gmail.com>
2015-01-05 17:34:49 +0530

    Updated parameters for deploy

--- app/recipes/deploy.rb
+++ app/recipes/deploy.rb
@@ -1,6 +1,14 @@
-deploy "/home/sysadmin/chef-repo/destination" do
-repository "/home/sysadmin/chef-repo/source/sample-play-app"
-symlink_before_migrate ({})
-symlinks ({})
-	
+#deploy the repo
+
+deploy_path = "/srv/www/immweb/" #path where the application needs to be deployed
+repository_path = "/home/jenkin/code" #path where the application resides (GIT or SVN or GITLAB)
+
+deploy deploy_path do
+	user "deploy"
+	group "apache"
+	repository repository_path
+	symlink_before_migrate ({})
+	symlinks ({})
+
+	restart_command "/etc/init.d/httpd restart"
 end
