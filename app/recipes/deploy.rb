42fec56067648efc1a4c234786b9454970c163fc
suhas <suhasg87@gmail.com>
2015-01-06 17:24:26 +0530

    updated deploy script

--- app/recipes/deploy.rb
+++ app/recipes/deploy.rb
@@ -3,12 +3,15 @@
 deploy_path = "/srv/www/immweb/" #path where the application needs to be deployed
 repository_path = "/home/jenkin/code" #path where the application resides (GIT or SVN or GITLAB)
 
-deploy deploy_path do
-	user "deploy"
-	group "apache"
-	repository_cache repository_path
-	symlink_before_migrate ({})
-	symlinks ({})
-
-	restart_command "/etc/init.d/httpd restart"
+script "Deploy code to document root" do
+	code <<-EOH
+	set -xv
+	DAT=`date '+%Y%m%d%H%M%S'`
+	mkdir -p #{deploy_path}/releases/$DAT
+	mkdir -p #{deploy_path}/current
+	rsync #{repository_path}/* #{deploy_path}/releases/$DAT
+	chown -R deploy:apache #{deploy_path}/*
+	ln -s #{deploy_path}/releases/$DAT/* #{deploy_path}/current
+	/etc/init.d/httpd restart
+	EOH
 end
