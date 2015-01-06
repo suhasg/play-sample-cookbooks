1fd8da390d55ad7665869f4638e9f7867bb0253b
suhas <suhasg87@gmail.com>
2015-01-06 17:45:28 +0530

    Update deploy.rb

--- app/recipes/deploy.rb
+++ app/recipes/deploy.rb
@@ -5,11 +5,10 @@ repository_path = "/home/jenkin/code" #path where the application resides (GIT o
 
 script "Deploy code to document root" do
 	code <<-EOH
-	set -xv
 	DAT=`date '+%Y%m%d%H%M%S'`
 	mkdir -p #{deploy_path}/releases/$DAT
 	mkdir -p #{deploy_path}/current
-	rsync #{repository_path}/* #{deploy_path}/releases/$DAT
+	cp -r #{repository_path}/* #{deploy_path}/releases/$DAT
 	chown -R deploy:apache #{deploy_path}/*
 	ln -s #{deploy_path}/releases/$DAT/* #{deploy_path}/current
 	/etc/init.d/httpd restart
